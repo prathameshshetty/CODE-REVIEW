51933c4345a2f9306c72de3622b4fadd5e28469c
Your New Name <prathamesh.cse@gmail.com>
2025-07-04 17:05:57 +0530

    Updated the Frontend

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const app = express();
 const aiRoutes = require("./routes/ai.routes");
+const cors=require('cors');
 
+const app = express();
+
+app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => {
